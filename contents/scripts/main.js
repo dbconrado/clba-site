ec5280b2714393b0ca19063e5ff3e7401041684f
Daniel Conrado <dbconrado@gmail.com>
2019-08-10 20:40:49 -0300

    posicionando a imagem de fundo para melhor visualização

--- contents/scripts/main.js
+++ contents/scripts/main.js
@@ -8,4 +8,12 @@ ready(function() {
         autoplay: true,
         autoplaySpeed: 5000
     });
+
+    /* exibir mais a imagem da capa */
+    /*var headerHeight = $('header').height();
+    var imgHeight = $('#imgfundo').height();
+    var factor = imgHeight * 0.30;
+    var fillSpace = imgHeight - headerHeight - factor;
+
+    $('header').css('margin-bottom', fillSpace);*/
 });
